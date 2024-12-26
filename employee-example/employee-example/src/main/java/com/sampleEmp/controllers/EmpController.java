9c5f32eb26ee2e2f60e7f7fd9f18a3205728c6ee
goutham147 <gouthamteja04@gmail.com>
2024-12-26 20:16:11 +0530

    getEmployeeById added

--- employee-example/employee-example/src/main/java/com/sampleEmp/controllers/EmpController.java
+++ employee-example/employee-example/src/main/java/com/sampleEmp/controllers/EmpController.java
@@ -1,9 +1,11 @@
 package com.sampleEmp.controllers;
 
+import com.sampleEmp.EmployeeExampleApplication;
 import com.sampleEmp.entities.Employee;
 import com.sampleEmp.services.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class EmpController {
     @GetMapping
     public List<Employee> getAllEmployee(){
         return empService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable int id){
+        Optional<Employee> employee = empService.findById(id);
+        return employee.orElse(null);
     }
 }
